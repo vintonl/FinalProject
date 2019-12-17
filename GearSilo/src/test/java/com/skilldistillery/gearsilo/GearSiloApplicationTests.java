a29d2cfaf07ce32f34e51cd90e792fe6fa694de1
Colt <coltaustin45@hotmail.com>
2019-12-17 15:28:25 -0700

    Stub Boot project, linked JPA to Boot

---
+++ GearSilo/src/test/java/com/skilldistillery/gearsilo/GearSiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.gearsilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearSiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
