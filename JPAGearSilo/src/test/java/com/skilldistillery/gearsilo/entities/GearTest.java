6df043394d4a8a529f8fa6a255beca1c1add6986
Vinton Lee <vinton.e.lee@gmail.com>
2019-12-18 10:48:32 -0700

    gear entity and test completed

---
+++ JPAGearSilo/src/test/java/com/skilldistillery/gearsilo/entities/GearTest.java
@@ -0,0 +1,58 @@
+package com.skilldistillery.gearsilo.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GearTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Gear gear;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("GearSiloPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		gear = em.find(Gear.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		gear = null;
+		em.close();
+	}
+
+	@Test
+	@DisplayName("test primary fields")
+	void test_Post_entity__mappings() {
+		assertNotNull(gear);
+		assertEquals(1, gear.getId());
+		assertEquals("Surf Board", gear.getName());
+		assertEquals("New", gear.getCondition());
+		assertEquals(50.0, gear.getPrice());
+		assertNull(gear.getDescription());
+		assertNull(gear.getImageUrl());
+		assertTrue(gear.getAvailable());
+		assertTrue(gear.getActive());
+	}
+
+}
