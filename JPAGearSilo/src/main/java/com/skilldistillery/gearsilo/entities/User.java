e5825d4bdef7fdda3ac6ca23a5da1b94059bdfec
Briman-Jag <brianstreetman25@gmail.com>
2019-12-18 09:28:50 -0700

    Changed username to email in User class. UserTest is green

--- JPAGearSilo/src/main/java/com/skilldistillery/gearsilo/entities/User.java
+++ JPAGearSilo/src/main/java/com/skilldistillery/gearsilo/entities/User.java
@@ -11,12 +11,12 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	private String username;
+	private String email;
 
-	public User(int id, String username) {
+	public User(int id, String email) {
 		super();
 		this.id = id;
-		this.username = username;
+		this.email = email;
 	}
 
 	public User() {
@@ -31,20 +31,21 @@ public class User {
 		this.id = id;
 	}
 
-	public String getUsername() {
-		return username;
+
+	public String getEmail() {
+		return email;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + id;
-		result = prime * result + ((username == null) ? 0 : username.hashCode());
 		return result;
 	}
 
@@ -57,19 +58,19 @@ public class User {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		if (id != other.id)
-			return false;
-		if (username == null) {
-			if (other.username != null)
+		if (email == null) {
+			if (other.email != null)
 				return false;
-		} else if (!username.equals(other.username))
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + "]";
+		return "User [id=" + id + ", email=" + email + "]";
 	}
 
 }
