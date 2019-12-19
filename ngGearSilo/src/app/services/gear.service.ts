4aeb380effdf5f84beeb5b1f635f1229c0de289a
Vinton Lee <vinton.e.lee@gmail.com>
2019-12-19 14:19:23 -0700

    created login, logout, register, auth services, added enviroment

--- ngGearSilo/src/app/services/gear.service.ts
+++ ngGearSilo/src/app/services/gear.service.ts
@@ -1,9 +1,44 @@
+import { Gear } from './../models/gear';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GearService {
+  private baseUrl = environment.baseUrl;
+  private url = this.baseUrl + 'api/beverages';
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router, private authService: AuthService) { }
+
+  index() {
+
+    if (!this.authService.checkLogin()) {
+      return null;
+    }
+    const httpOptions = {
+      headers: new HttpHeaders({
+        // 'Content-Type': 'application/json',
+        Authorization: 'Basic' + this.authService.getCredentials(), 'X-Requested-With': 'XMLHttpRequest'
+      })
+    };
+
+    return this.http.get<Gear[]>(this.url)
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('BeverageService.index() Error');
+        })
+      );
+  }
+
+
+
+
+
 }
