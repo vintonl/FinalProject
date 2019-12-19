edc6bb876c6a8cf177c0380960e50bc15cdd6077
Adam <adamonwan@gmail.com>
2019-12-19 16:19:00 -0700

    review of gear

--- GearSilo/src/main/java/com/skilldistillery/gearsilo/services/ReviewOfGearServiceImpl.java
+++ GearSilo/src/main/java/com/skilldistillery/gearsilo/services/ReviewOfGearServiceImpl.java
@@ -1,11 +1,13 @@
 package com.skilldistillery.gearsilo.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.gearsilo.entities.ReviewOfGear;
+import com.skilldistillery.gearsilo.entities.User;
 import com.skilldistillery.gearsilo.repositories.ReviewOfGearRepository;
 import com.skilldistillery.gearsilo.repositories.UserRepository;
 
@@ -18,14 +20,30 @@ public class ReviewOfGearServiceImpl implements ReviewOfGearService {
 	@Autowired
 	private UserRepository userRepo;
 	
-	public List<ReviewOfGear> showMyReviewOfGear(String username) {
-		return reviewOfGearRepo.findByUser_Username(username);
-	}
+	public List<ReviewOfGear> findAll(String username, int id) {
 
-	@Override
-	public List<ReviewOfGear> findAll(String name, int uid) {
-		// TODO Auto-generated method stub
+		User user = userRepo.findUserByUsername(username);
+		List<ReviewOfGear> results = new ArrayList<>();
+		List<ReviewOfGear> gearReviews = new ArrayList<>();
+
+		if (user.getId() == id || user.getRole().equals("admin")) {
+
+			gearReviews = reviewOfGearRepo.findAll();
+
+			for (ReviewOfGear reviewOfGear : gearReviews) {
+
+				if (reviewOfGear.getReservation().getGearId().getId() == id) {
+
+					results.add(reviewOfGear);
+
+				}
+
+			}
+			return results;
+
+		}
 		return null;
+
 	}
 	
 }
