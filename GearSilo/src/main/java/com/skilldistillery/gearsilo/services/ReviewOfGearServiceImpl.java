ccbe3b94ccbc76cfbd69473d53a0ff3564d51291
Adam <adamonwan@gmail.com>
2019-12-19 15:54:12 -0700

    deleted t/s lines in gear controller

--- GearSilo/src/main/java/com/skilldistillery/gearsilo/services/ReviewOfGearServiceImpl.java
+++ GearSilo/src/main/java/com/skilldistillery/gearsilo/services/ReviewOfGearServiceImpl.java
@@ -1,13 +1,25 @@
 package com.skilldistillery.gearsilo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.gearsilo.entities.ReviewOfGear;
 import com.skilldistillery.gearsilo.repositories.ReviewOfGearRepository;
+import com.skilldistillery.gearsilo.repositories.UserRepository;
 
 @Service
 public class ReviewOfGearServiceImpl implements ReviewOfGearService {
 
 	@Autowired
 	private ReviewOfGearRepository reviewOfGearRepo;
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	public List<ReviewOfGear> showMyReviewOfGear(String username) {
+		return reviewOfGearRepo.findByUser_Username(username);
+	}
+	
 }
