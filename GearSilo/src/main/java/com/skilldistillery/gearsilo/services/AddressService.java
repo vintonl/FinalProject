f4733d1115e3c8bcf757569a027b376153c9cc15
Briman-Jag <brianstreetman25@gmail.com>
2019-12-18 17:32:54 -0700

    Added CRUD to AddressService & AddressServiceImpl

--- GearSilo/src/main/java/com/skilldistillery/gearsilo/services/AddressService.java
+++ GearSilo/src/main/java/com/skilldistillery/gearsilo/services/AddressService.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.gearsilo.services;
 
-public interface AddressService {
+import java.util.List;
 
+import com.skilldistillery.gearsilo.entities.Address;
+
+public interface AddressService {
+	
+	public List<Address> allAddresses();
+	
+	public Address showAddress(Integer addressId);
+	
+	public Address createAddress(Address address);
+	
+	public Address update(Address address, Integer addressId);
+	
+	public boolean delete(Integer id);
+	
 }
