ed635392fd208d545d0ce31227b4b411d60bdd5e
Adam <adamonwan@gmail.com>
2019-12-19 16:13:09 -0700

    application runs again

--- GearSilo/src/main/java/com/skilldistillery/gearsilo/controllers/ReviewOfGearController.java
+++ GearSilo/src/main/java/com/skilldistillery/gearsilo/controllers/ReviewOfGearController.java
@@ -9,10 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.skilldistillery.gearsilo.entities.Gear;
+import com.skilldistillery.gearsilo.entities.ReviewOfGear;
 import com.skilldistillery.gearsilo.services.ReviewOfGearService;
 
 @RestController
@@ -23,8 +24,18 @@ public class ReviewOfGearController {
 	@Autowired
 	private ReviewOfGearService reviewGearSvc;
 	
-//	@GetMapping("users/{uid}/reviews")
-//	public List<Gear> index (HttpServletRequest req, HttpServletResponse res, Principal principal) {
-//		return reviewGearSvc.showMyReviewOfGear(principal.getName());
-//	}
+	@GetMapping("users/{uid}/reviews")
+	public List<ReviewOfGear> index (@PathVariable int uid, HttpServletRequest req,
+			HttpServletResponse resp, Principal principal) {
+		List<ReviewOfGear> gearReview = reviewGearSvc.findAll(principal.getName(), uid);
+
+		if (gearReview != null && gearReview.size() == 0) {
+			resp.setStatus(204);
+		}
+
+		if (gearReview == null) {
+			resp.setStatus(404);
+		}
+		return gearReview;
+	}	
 }
