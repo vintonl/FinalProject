4e94c087433431ba8cf69da1f5f499242e5445da
Briman-Jag <brianstreetman25@gmail.com>
2019-12-19 17:16:24 -0700

    Made edits to ReviewOfLenderReository and ServicesImpl

--- GearSilo/src/main/java/com/skilldistillery/gearsilo/repositories/ReviewOfLenderRepository.java
+++ GearSilo/src/main/java/com/skilldistillery/gearsilo/repositories/ReviewOfLenderRepository.java
@@ -1,17 +1,17 @@
 package com.skilldistillery.gearsilo.repositories;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.skilldistillery.gearsilo.entities.Reservation;
 import com.skilldistillery.gearsilo.entities.ReviewOfLender;
-import com.skilldistillery.gearsilo.entities.User;
 
 public interface ReviewOfLenderRepository extends JpaRepository<ReviewOfLender, Integer> {
 
 
-
+	public Optional<ReviewOfLender> findByReservation_ShopperUserId(String username, int shopId);
 	
-
+	public Reservation findReservationById(String username, int resId);
 	
 }
