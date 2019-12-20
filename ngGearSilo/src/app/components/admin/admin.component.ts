86b4b98e00a24e46c07e8d453e5625dcd11b1dad
Jerry Rogers <jerryrogers@Students-MacBook-Pro.local>
2019-12-20 10:22:22 -0700

    wk

--- ngGearSilo/src/app/components/admin/admin.component.ts
+++ ngGearSilo/src/app/components/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-admin',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+
+  allUsers: User[] = [];
 
   constructor() { }
 
