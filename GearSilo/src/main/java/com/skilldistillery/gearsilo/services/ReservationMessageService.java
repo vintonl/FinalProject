f73a1fddbe2484bd1da5c33fc3dacacdc5b1d514
Briman-Jag <brianstreetman25@gmail.com>
2019-12-20 09:46:17 -0700

    Added findAll, findById, and create methods for ReservationMessage service

--- GearSilo/src/main/java/com/skilldistillery/gearsilo/services/ReservationMessageService.java
+++ GearSilo/src/main/java/com/skilldistillery/gearsilo/services/ReservationMessageService.java
@@ -1,5 +1,20 @@
 package com.skilldistillery.gearsilo.services;
 
+import java.util.List;
+
+import com.skilldistillery.gearsilo.entities.ReservationMessage;
+
 public interface ReservationMessageService {
+	
+	public List<ReservationMessage> findAll(String username);
+	
+	public ReservationMessage findReservationMessageById(String username, int resMsgId);
+
+	public ReservationMessage createReservationMessage(String username, ReservationMessage resMsg, int id, int resId);
+		
+	public ReservationMessage updateReservationMessage(String username, ReservationMessage resMsg, int resMsgId);
+	
+	public ReservationMessage deleteReservation(String username, int resMsg);
+
 
 }
--- GearSilo/src/main/java/com/skilldistillery/gearsilo/services/ReservationMessageServiceImpl.java
+++ GearSilo/src/main/java/com/skilldistillery/gearsilo/services/ReservationMessageServiceImpl.java
@@ -1,13 +1,83 @@
 package com.skilldistillery.gearsilo.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.gearsilo.entities.Reservation;
+import com.skilldistillery.gearsilo.entities.ReservationMessage;
+import com.skilldistillery.gearsilo.entities.User;
 import com.skilldistillery.gearsilo.repositories.ReservationMessageRepository;
+import com.skilldistillery.gearsilo.repositories.ReservationRepository;
+import com.skilldistillery.gearsilo.repositories.UserRepository;
 
 @Service
 public class ReservationMessageServiceImpl implements ReservationMessageService {
 
 	@Autowired
-	private ReservationMessageRepository resMessRepo;
+	private ReservationMessageRepository resMsgRepo;
+	
+	@Autowired 
+	private UserRepository userRepo;
+	
+	@Autowired
+	private ReservationRepository resRepo;
+
+	@Override
+	public List<ReservationMessage> findAll(String username) {
+		
+		return resMsgRepo.findAll();
+	}
+
+	@Override
+	public ReservationMessage findReservationMessageById(String username, int resMsgId) {
+		
+		ReservationMessage resMsg = null;
+		Optional<ReservationMessage> opt = resMsgRepo.findById(resMsgId);
+		if (opt.isPresent()) {
+			resMsg = opt.get();
+			return resMsg;
+		} else {
+			return null;
+		}
+
+	}
+
+	@Override
+	public ReservationMessage createReservationMessage(String username, ReservationMessage resMsg, int id, int resId) {
+		 User user = userRepo.findUserByUsername(username);
+		 
+		 Optional<Reservation> resOpt = resRepo.findById(resId);
+		 Reservation reservation;
+		 
+		 if(resOpt.isPresent()) {
+			 reservation = resOpt.get();
+			 
+			 System.out.println(reservation);
+			 
+				if (user.getId() == id || user.getRole().equals("admin")) {
+					List<Reservation> reservations = user.getReservations();
+					System.err.println("number of reservations in user: " + reservations.size());
+					resMsg.setReservation(reservation);
+				}
+				resMsgRepo.saveAndFlush(resMsg);
+		 }
+		
+		
+		return resMsg;
+	}
+
+	@Override
+	public ReservationMessage updateReservationMessage(String username, ReservationMessage resMsg, int resMsgId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReservationMessage deleteReservation(String username, int resMsg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
