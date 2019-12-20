3ea52aee0054174b3cd0ce8460042370ab579b08
Adam <adamonwan@gmail.com>
2019-12-19 17:33:17 -0700

    review of gear

--- GearSilo/src/main/java/com/skilldistillery/gearsilo/controllers/ReviewOfGearController.java
+++ GearSilo/src/main/java/com/skilldistillery/gearsilo/controllers/ReviewOfGearController.java
@@ -10,9 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.gearsilo.entities.Gear;
 import com.skilldistillery.gearsilo.entities.ReviewOfGear;
 import com.skilldistillery.gearsilo.services.ReviewOfGearService;
 
@@ -38,4 +41,42 @@ public class ReviewOfGearController {
 		}
 		return gearReview;
 	}	
+	
+	@PostMapping("user/{uid}/reservation/{rid}/reviews/gearreviews")
+	public ReviewOfGear createGearReview(@RequestBody ReviewOfGear gearReview, @PathVariable int uid,
+			@PathVariable int rid, HttpServletRequest req, HttpServletResponse res, Principal principal) {
+	
+		try {
+			reviewGearSvc.createReviewOfGear(principal.getName(), gearReview, uid, rid);
+			res.setStatus(201);
+			StringBuffer url = req.getRequestURL();
+			res.addHeader("Location", url.toString());
+		} catch (Exception e) {
+			res.setStatus(400);
+			e.printStackTrace();
+		}
+		return gearReview;
+	}
+	
+//	@PostMapping("users/{uid}/gears")
+//	public Gear create(HttpServletRequest req, HttpServletResponse res, Principal principal, @RequestBody Gear gear,
+//			@PathVariable("uid") int uid) {
+//		try {
+//			gear = gearSvc.addGear(principal.getName(), gear);
+//			if (gear == null) {
+//				res.setStatus(401);
+//			} else {
+//				res.setStatus(201);
+//				StringBuffer url = req.getRequestURL();
+//				url.append("/").append(gear.getId());
+//				res.addHeader("Location", url.toString());
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			res.setStatus(500);
+//			gear = null;
+//		}
+//
+//		return gear;
+//	}
 }
