fa08b592347e2d5b42231bfbb1e34df65353df3d
Vinton Lee <vinton.e.lee@gmail.com>
2019-12-23 11:06:47 -0700

    regigistration works again without anm address

--- GearSilo/src/main/java/com/skilldistillery/gearsilo/controllers/AuthController.java
+++ GearSilo/src/main/java/com/skilldistillery/gearsilo/controllers/AuthController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
