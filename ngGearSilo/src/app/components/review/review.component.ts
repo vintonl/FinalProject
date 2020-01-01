fe389f6b07854a51c35d46398d8d4e44bd50872d
Vinton Lee <vinton.e.lee@gmail.com>
2020-01-01 14:32:15 -0700

    review of gear started, works, but may need more testing

--- ngGearSilo/src/app/components/review/review.component.ts
+++ ngGearSilo/src/app/components/review/review.component.ts
@@ -1,4 +1,12 @@
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { ReviewOfShopperService } from './../../services/review-of-shopper.service';
+import { Reservation } from './../../models/reservation';
+import { ReviewOfGear } from './../../models/review-of-gear';
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+
 
 @Component({
   selector: 'app-review',
@@ -7,9 +15,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private reviewOfShopperSvc: ReviewOfShopperService, private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
+    const cred = this.authService.getCredentials();
+
+    if (cred === null) {
+      this.router.navigateByUrl('/login');
+
+    }
   }
+
+
+  createGearReview(gearReview: NgForm) {
+    const newGearReview = {
+      rating: gearReview.value.rating,
+      review: gearReview.value.review,
+      reservation: {
+        id: '1'
+      }
+    };
+
+    let user = new User();
+
+    this.authService.getUserByUsername(this.authService.getLoggedInUsername()).subscribe(
+      good => {
+        user = good;
+        console.log(user);
+        this.reviewOfShopperSvc.createGearReview(newGearReview, user).subscribe(
+          next => {
+            console.log('ReviewComponent.createGearReview(): review of gear created.');
+            this.router.navigateByUrl('/profile');
+            console.log(next);
+          },
+          error => {
+            console.error('ReviewComponent.createGearReview(): error createGearReview.');
+            console.log(error);
+          }
+        );
+      },
+      error => {
+        console.log('ReviewOfShopperService.create() Error getting logged in user while creating gear review');
+      }
+    );
+  }
+
 
 }
