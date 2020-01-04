156c8b4e6166b5ea78119ca0a55530e6375132ed
Vinton Lee <vinton.e.lee@gmail.com>
2020-01-04 12:57:38 -0700

    cleaning console.log

--- ngGearSilo/src/app/services/auth.service.ts
+++ ngGearSilo/src/app/services/auth.service.ts
@@ -1,22 +1,21 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { environment } from "src/environments/environment";
-import { catchError, tap } from "rxjs/operators";
-import { Observable, throwError } from "rxjs";
-import { User } from "../models/user";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { catchError, tap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class AuthService {
   private baseUrl = environment.baseUrl;
-  // private user: User = new User();
 
   userLoggedIn = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  userLogInCheck () {
+  userLogInCheck() {
     if (this.getCredentials) {
       return true;
     } else {
@@ -26,11 +25,6 @@ export class AuthService {
 
   login(username, password) {
     this.userLoggedIn = true;
-    // this.user = new User();
-    // this.getUserByUsername(username).subscribe(data => this.user = data);
-    // console.log('in login');
-    // console.log(this.user);
-    // console.log(username);
 
     // Make credentials
     const credentials = this.generateBasicAuthCredentials(username, password);
@@ -38,46 +32,41 @@ export class AuthService {
     const httpOptions = {
       headers: new HttpHeaders({
         Authorization: `Basic ${credentials}`,
-        "X-Requested-With": "XMLHttpRequest"
+        'X-Requested-With': 'XMLHttpRequest'
       })
     };
 
     // create request to authenticate credentials
-    return this.http.get(this.baseUrl + "authenticate", httpOptions).pipe(
+    return this.http.get(this.baseUrl + 'authenticate', httpOptions).pipe(
       tap(res => {
-        localStorage.setItem("credentials", credentials);
-        localStorage.setItem("username", username);
+        localStorage.setItem('credentials', credentials);
+        localStorage.setItem('username', username);
         return res;
       }),
       catchError((err: any) => {
-        console.log(err);
-        return throwError("AuthService.login(): Error logging in.");
+        // console.log(err);
+        return throwError('AuthService.login(): Error logging in.');
       })
     );
   }
 
   register(user) {
-    // create request to register a new account
-
-    console.log(user);
-
-    return this.http.post(this.baseUrl + "register", user).pipe(
+    return this.http.post(this.baseUrl + 'register', user).pipe(
       catchError((err: any) => {
-        console.log(err);
-        return throwError("AuthService.register(): error registering user.");
+        // console.log(err);
+        return throwError('AuthService.register(): error registering user.');
       })
     );
   }
 
   logout() {
     this.userLoggedIn = false;
-    // this.user = new User();
-    localStorage.removeItem("credentials");
-    localStorage.removeItem("username");
+    localStorage.removeItem('credentials');
+    localStorage.removeItem('username');
   }
 
   checkLogin() {
-    if (localStorage.getItem("credentials")) {
+    if (localStorage.getItem('credentials')) {
       return true;
     }
     return false;
@@ -88,37 +77,30 @@ export class AuthService {
   }
 
   getCredentials() {
-    return localStorage.getItem("credentials");
+    return localStorage.getItem('credentials');
   }
 
   getLoggedInUsername() {
-    return localStorage.getItem("username");
+    return localStorage.getItem('username');
   }
   getUserByUsername(username: string) {
-    //  localStorage.getItem();
-
-    console.log("in get user by username method");
-    console.log(username);
     const httpOptions = {
       headers: new HttpHeaders({
-        "Access-Control-Allow-Headers": "Content-Type",
+        'Access-Control-Allow-Headers': 'Content-Type',
         // 'Content-Type': 'application/json',
         Authorization: `Basic ` + this.getCredentials(),
-        "X-Requested-With": "XMLHttpRequest"
+        'X-Requested-With': 'XMLHttpRequest'
       })
     };
 
     return this.http
-      .get<User>(this.baseUrl + "api/users/" + username, httpOptions)
+      .get<User>(this.baseUrl + 'api/users/' + username, httpOptions)
       .pipe(
         catchError((err: any) => {
-          console.log(err);
-          return throwError("AuthService.register(): error registering user.");
+          // console.log(err);
+          return throwError('AuthService.register(): error registering user.');
         })
       );
   }
 
-  // getUser(): User {
-  //   return this.user;
-  // }
 }
